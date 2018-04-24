5b32fd3f0b7f9b2a2af368b670fbcf268bd3d325
aryaanandasyahfikri <38453147+aryaanandasyahfikri@users.noreply.github.com>
2018-04-24 15:50:35 +0700

    Add files via upload

---
+++ AryaAnandaSyahFikri/gethint.php
@@ -0,0 +1,32 @@
+<?php
+$a[] = "Arya Ananda Syah Fikri";
+$a[] = "Bartra";
+$a[] = "Crespo";
+$a[] = "David";
+$a[] = "Ezequiel";
+$a[] = "Francesco";
+$a[] = "Gilardino";
+$a[] = "Hummels";
+$a[] = "Ilkay";
+$a[] = "Jerome";
+
+
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+if ($q !== "") {
+	$q = strtolower($q);
+	$len=strlen($q);
+	foreach($a as $name) {
+		if (stristr($q, substr($name, 0, $len))){
+			if ($hint === "") {
+				$hint = $name;
+				}else{
+					$hint .=", $name";
+				}
+			}
+		}
+	}
+echo $hint === "" ? "no suggestion" : $hint;
+?>
